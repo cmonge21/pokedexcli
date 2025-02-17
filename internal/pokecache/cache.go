8666826a4e4046c1038168f46355d05c6dd6a396
cmonge21 <carlypres@gmail.com>
2025-02-17 19:59:38 -0400

    Working on cache

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -2,15 +2,49 @@ package pokecache
 
 import (
 	"sync"
+	"time"
 )
 
-tyoe Cache struct {
+type cacheEntry struct {
 	createdAt time.Time
-	val []byte
+	val       []byte
 }
 
-func NewCach() *Cache {
-	return &Cache{
-		data: make(map[string]cacheEntry),
+type Cache struct {
+	data     map[string]cacheEntry
+	mutex    sync.Mutex // Protects the map
+	interval time.Duration
+
+func NewCache(interval time.Duration) *Cache {
+	cache := &Cache{
+		data:     make(map[string]cacheEntry),
+		interval: interval,
 	}
+	go cache.reapLoop() 
+	return cache
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop() 
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.data[key] = cacheEntry{
+        createdAt: time.Now(),
+        val:       val,
+    }
+}
+
+func (c *Cache) Get(key string)) ([]byte, bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	entry, exists := c.data[key]
+	if !exists {
+		return nil, false
+	}
+	return entry.val, true
 }
